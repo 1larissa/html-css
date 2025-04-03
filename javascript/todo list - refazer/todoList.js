22276d01ee9c55f5804584b80ce288a208bb2e46
Larissa Rodrigues <larissarodrigues@alunos.utfpr.edu.br>
2025-04-03 08:10:04 -0300

    add cute love font + background clouds

--- javascript/todo list - refazer/todoList.js
+++ javascript/todo list - refazer/todoList.js
@@ -9,8 +9,6 @@
 
 // renderList();
 
-
-
 // function addTodo() {
 //     let inputElement = document.querySelector('.js-name-input');
 //     let name = inputElement.value;
@@ -20,7 +18,7 @@
 //     todoList.push({
 //         // name: name,
 //         // dueDate: dueDate
-//         // ou 
+//         // ou
 //         name,
 //         dueDate
 //     });
@@ -58,67 +56,72 @@
 //     })
 // }
 
-
 // function saveLocalStorageTodoList(object) {
 
 //     localStorage.setItem('todoList', JSON.stringify(object));
 // }
 
-const todoList = [{
-    name: 'make dinner',
-    dueDate: '2022-12-22'
-  }, {
-    name: 'wash dishes',
-    dueDate: '2022-12-22'
-  }];
-  
-  renderTodoList();
-  
-  function renderTodoList() {
-    let todoListHTML = '';
-  
-    todoList.forEach((todoObject, index) => {
-      const { name, dueDate } = todoObject;
-      const html = `
+const todoList = [
+  {
+    name: "make dinner",
+    dueDate: "2022-12-22",
+  },
+  {
+    name: "wash dishes",
+    dueDate: "2022-12-22",
+  },
+];
+
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHTML = "";
+
+  todoList.forEach((todoObject, index) => {
+    const { name, dueDate } = todoObject;
+    const html = `
         <div>${name}</div>
         <div>${dueDate}</div>
-        <button class="delete-todo-button js-delete-todo-button">Delete</button> 
+        <button class="delete-todo-button js-delete-todo-button">
+          <span class="material-symbols-outlined">
+          delete
+          </span>
+        </button> 
       `;
-      todoListHTML += html;
-    });
-  
-    document.querySelector('.js-todo-list')
-      .innerHTML = todoListHTML;
-  
-    document.querySelectorAll('.js-delete-todo-button')
-      .forEach((deleteButton, index) => {
-        deleteButton.addEventListener('click', () => {
-          todoList.splice(index, 1);
-          renderTodoList();
-        });
+    todoListHTML += html;
+  });
+
+  document.querySelector(".js-todo-list").innerHTML = todoListHTML;
+
+  document
+    .querySelectorAll(".js-delete-todo-button")
+    .forEach((deleteButton, index) => {
+      deleteButton.addEventListener("click", () => {
+        todoList.splice(index, 1);
+        renderTodoList();
       });
-  }
-  
-  document.querySelector('.js-add-todo-button')
-    .addEventListener('click', () => {
-      addTodo();
     });
-  
-  function addTodo() {
-    const inputElement = document.querySelector('.js-name-input');
-    const name = inputElement.value;
-  
-    const dateInputElement = document.querySelector('.js-due-date-input');
-    const dueDate = dateInputElement.value;
-  
-    todoList.push({
-      //name: name,
-      //dueDate: dueDate,
-      name,
-      dueDate
-    });
-  
-    inputElement.value = '';
-  
-    renderTodoList();
-  }
+}
+
+document.querySelector(".js-add-todo-button").addEventListener("click", () => {
+  addTodo();
+});
+
+function addTodo() {
+  const inputElement = document.querySelector(".js-name-input");
+  const name = inputElement.value;
+
+  const dateInputElement = document.querySelector(".js-due-date-input");
+  const dueDate = dateInputElement.value;
+
+  todoList.push({
+    //name: name,
+    //dueDate: dueDate,
+    name,
+    dueDate,
+  });
+
+  inputElement.value = "";
+
+  renderTodoList();
+}
